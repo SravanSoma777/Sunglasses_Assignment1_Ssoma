9e41d5f82b6cb425c03282b08ac9450411665ba0
SravanSoma777 <W0766138@myscc.ca>
2022-10-12 12:21:03 -0400

    added a class file called sunglasses and defined properties or attributes

---
+++ Models/Sunglasses.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sunglasses_Assignment1_Ssoma.Models
+{
+    public class Sunglasses
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string ShadeType { get; set; }
+        public decimal Price { get; set; }
+
+        public string review { get; set; }
+
+        public string country { get; set; }
+    }
+}
